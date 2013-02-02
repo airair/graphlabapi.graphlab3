6a6d51c7cc2f6b7cf0f5e93ff42f8c7c6f6f9cf5
Svilen Mihaylov <Svilen Mihaylov@localhost>
2013-02-01 17:02:41 -0800

    Added Eigen to build. Matrix and Vector datatypes

--- src/graphlab/database/basic_types.hpp
+++ src/graphlab/database/basic_types.hpp
@@ -1,5 +1,6 @@
 #ifndef GRAPHLAB_DATABASE_BASIC_TYPES_HPP
 #define GRAPHLAB_DATABASE_BASIC_TYPES_HPP
+#include <Eigen/Dense>
 #include <string>
 #include <stdint.h>
 namespace graphlab {
@@ -25,7 +26,21 @@ typedef std::string graph_string_t;
 /// blob field data type
 typedef std::string graph_blob_t;
 
+/**
+ * \brief We use the eigen library's vector type to represent
+ * mathematical vectors.
+ */
+typedef Eigen::VectorXd graphlab_vecd_t;
+typedef Eigen::VectorXcd graphlab_veccd_t;
+typedef Eigen::VectorXi graphlab_veci_t;
 
+/**
+ * \brief We use the eigen library's matrix type to represent
+ * matrices.
+ */
+typedef Eigen::MatrixXd graphlab_matd_t;
+typedef Eigen::MatrixXcd graphlab_matcd_t;
+typedef Eigen::MatrixXi graphlab_mati_t;
 
 /// \ingroup group_graph_database
 /// Possible data types for fields
@@ -34,9 +49,14 @@ enum graph_datatypes_enum {
 	INT_TYPE,
 	DOUBLE_TYPE,
 	STRING_TYPE,
-	BLOB_TYPE
+	BLOB_TYPE,
+	VECI_TYPE,
+	VECD_TYPE,
+	VECCD_TYPE,
+	MATI_TYPE,
+	MATD_TYPE,
+	MATCD_TYPE
 };
-
 
 /// \ingroup group_graph_database
 inline bool is_scalar_graph_datatype(graph_datatypes_enum e) {

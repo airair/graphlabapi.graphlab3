3bfaf7105a27412894c574bba6114f0a91f14840
Haijie Gu <gu.haijie@gmail.com>
2013-02-04 12:04:21 -0800

    add edge test in sharedmem database

--- tests/graph_database_sharedmem_test.cpp
+++ tests/graph_database_sharedmem_test.cpp
@@ -18,6 +18,7 @@ void testAddVertex() {
   ASSERT_EQ(db.num_vertices(), 1);
   ASSERT_TRUE(data->is_vertex());
 
+  // return false when adding the same vertex.
   ASSERT_FALSE(db.add_vertex(0));
   ASSERT_TRUE(db.add_vertex(1));
   ASSERT_EQ(db.num_vertices(), 2);
@@ -36,7 +37,8 @@ void testAddEdge() {
   db.add_edge(1,3);
   db.add_edge(1,4);
   db.add_edge(3,4);
-  ASSERT_EQ(db.num_edges(),4);
+  db.add_edge(4,1);
+  ASSERT_EQ(db.num_edges(),5);
   ASSERT_EQ(db.num_vertices(),4);
 
   graphlab::graph_vertex* v = db.get_vertex(1);
@@ -45,17 +47,19 @@ void testAddEdge() {
   size_t totalin = 0;
   size_t totalout = 0;
   for (size_t i = 0 ; i < nshards; ++i) {
-    v->get_adj_list(0, true, &inedges, &outedges);
+    v->get_adj_list(i, true, &inedges, &outedges);
     totalin += inedges.size();
     totalout += outedges.size();
     db.free_edge_vector(&inedges);
     db.free_edge_vector(&outedges);
   }
-  ASSERT_EQ(totalin, 3); 
-  ASSERT_EQ(totalout, 0); 
+  ASSERT_EQ(totalin, 1); 
+  ASSERT_EQ(totalout, 3); 
+
 }
 
 int main(int argc, char** argv) {
   testAddVertex();
+  testAddEdge();
   return 0;
 }

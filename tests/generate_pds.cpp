ab95c210ecdc39b0a3c00a13932484a7f29943cf
Yucheng Low <Yucheng Low@localhost>
2013-02-06 16:16:20 -0800

    Added some comments and renamed some variables in generate_pds.

--- tests/generate_pds.cpp
+++ tests/generate_pds.cpp
@@ -2,25 +2,28 @@
 #include <vector>
 using namespace std;
 bool test_seq(int a, int b, int c, int p, std::vector<int>& result) {
-  std::vector<int> res;
+  std::vector<int> seq;
   int pdslength = p*p + p + 1;
-  res.resize(pdslength + 3);
-  res[0] = 0; res[1] = 0; res[2] = 1;
+  seq.resize(pdslength + 3);
+  seq[0] = 0; seq[1] = 0; seq[2] = 1;
   int ctr = 2;
-  for (int i = 3; i < res.size(); ++i) {
-    res[i] = a * res[i - 1] + b * res[i - 2] + c * res[i - 3];
-    res[i] = res[i] % p;
-    ctr += (res[i] == 0);
+  for (int i = 3; i < seq.size(); ++i) {
+    seq[i] = a * seq[i - 1] + b * seq[i - 2] + c * seq[i - 3];
+    seq[i] = seq[i] % p;
+    ctr += (seq[i] == 0);
+    // PDS must be of length p + 1
+    // and are the 0's of seq.
     if (i < pdslength && ctr > p + 1) return false;
   }
-  if (res[pdslength] == 0 && res[pdslength + 1] == 0){ // && res[pdslength + 2] == 1) {
+  if (seq[pdslength] == 0 && seq[pdslength + 1] == 0){ 
     // we are good to go
     // now find the 0s
     for (int i = 0; i < pdslength; ++i) {
-      if (res[i] == 0) {
+      if (seq[i] == 0) {
         result.push_back(i);
       }
     }
+    // probably not necessary. but verify that the result has length p + 1
     if (result.size() != p + 1) {
       result.clear();
       return false;
@@ -34,19 +37,18 @@ bool test_seq(int a, int b, int c, int p, std::vector<int>& result) {
 
 
 std::vector<int> find_pds(int p) {
-  std::vector<int> res;
+  std::vector<int> result;
   for (int a = 0; a < p; ++a) {
     for (int b = 0; b < p; ++b) {
       if (b == 0 && a == 0) continue;
       for (int c = 1; c < p; ++c) {
-        res.clear();
-        if (test_seq(a,b,c,p,res)) {
-          return res;
+        if (test_seq(a,b,c,p,result)) {
+          return result;
         }
       }
     }
   } 
-  return res;
+  return result;
 }
 
 int main(int argc, char** argv) {
@@ -58,19 +60,19 @@ int main(int argc, char** argv) {
   int p = atoi(argv[1]);
   std::cout << "p = " << p << "\n";
   std::cout << "modulus = " << p*p+p+1 << "\n";
-  std::vector<int> res = find_pds(p);
-  std::cout << "PDS length = " << res.size() << "\n";
-  for (size_t i = 0;i < res.size(); ++i) {
-    std::cout << res[i] << "\t";
+  std::vector<int> result = find_pds(p);
+  std::cout << "PDS length = " << result.size() << "\n";
+  for (size_t i = 0;i < result.size(); ++i) {
+    std::cout << result[i] << "\t";
   }
   std::cout << "\n";
   // verify pdsness
   int pdslength = p *p + p + 1;
   std::vector<int> count(pdslength, 0);
-  for (int i = 0;i < res.size(); ++i) {
-    for (int j = 0;j < res.size(); ++j) {
+  for (int i = 0;i < result.size(); ++i) {
+    for (int j = 0;j < result.size(); ++j) {
       if (i == j) continue;
-      count[(res[i] - res[j] + pdslength) % pdslength]++;
+      count[(result[i] - result[j] + pdslength) % pdslength]++;
     }
   }
   bool ispds = true;

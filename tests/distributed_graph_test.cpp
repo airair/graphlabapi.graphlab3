5536a3ce2ccaae6a4c3d9b18601e8d7b6126b38a
Svilen Mihaylov <Svilen Mihaylov@localhost>
2013-02-20 15:28:54 -0800

    merge

---
+++ tests/distributed_graph_test.cpp
@@ -0,0 +1,8 @@
+#include <graphlab/database/distributed_graph/distributed_graph.hpp>
+#include <vector>
+using namespace std;
+
+int main(int argc, char** argv) {
+  graphlab::distributed_graph graph;
+  return 0;
+}

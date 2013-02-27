7c1c8374abc32bb6bae3a434d697e6641400eaa2
Svilen Mihaylov <Svilen Mihaylov@localhost>
2013-02-27 11:06:48 -0800

    modified test

--- tests/graph_database_test.cpp
+++ tests/graph_database_test.cpp
@@ -1,17 +1,18 @@
+#include <boost/date_time/posix_time/posix_time_types.hpp>
+
 #include <graphlab/database/graph_database.hpp>
 #include <graphlab/database/kvstore_mongodb.hpp>
 #include <graphlab/database/kvstore_mysql.hpp>
+#include <graphlab/database/kvstore_base.hpp>
 
-int main(int argc, char** argv) {
-  graphlab::kvstore_mongodb kv;
-//  graphlab::kvstore_mysql kv;
+void test_kv(graphlab::kvstore_base &kv) {
   graphlab::key_type k;
   graphlab::value_type v, v1;
   char testv[100];
   char long_key[65000];
 
   memset(long_key, 1, sizeof(long_key));
-  long_key[10000-1] = 0;
+  long_key[2000-1] = 0;
   v = long_key;
 
   for (int i = 0; i < 100000/*00*/; i++) {
@@ -34,12 +35,35 @@ int main(int argc, char** argv) {
   std::vector<graphlab::value_type> r;
 
   r = kv.range_get(0, 10);
-  printf("Got %d results\n", r.size());
+  printf("Got %ld results\n", r.size());
 
   kv.remove_all();
 
   r = kv.range_get(0, 10);
-  printf("Got %d results\n", r.size());
+  printf("Got %ld results\n", r.size());
+}
+
+typedef boost::posix_time::ptime Time;
+typedef boost::posix_time::time_duration TimeDuration;
+
+int main(int argc, char** argv) {
+  graphlab::kvstore_mongodb kv_mongo;
+  graphlab::kvstore_mysql kv_mysql;
+
+  Time t1, t2;
+  TimeDuration td;
+
+  printf("Runnung mongo test...\n");
+  t1 = boost::posix_time::microsec_clock::local_time();
+  test_kv(kv_mongo);
+  t2 = boost::posix_time::microsec_clock::local_time();
+  printf("Finished in %lld ms.\n", (t2-t1).total_milliseconds());
+
+  printf("Runnung mysql test...\n");
+  t1 = boost::posix_time::microsec_clock::local_time();
+  test_kv(kv_mysql);
+  t2 = boost::posix_time::microsec_clock::local_time();
+  printf("Finished in %lld ms.\n", (t2-t1).total_milliseconds());
 
   return 0;
 }
